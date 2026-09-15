3e0dcf10ec3c91d70180b3740fdfdda2fa4a42ab
akdawodu-vision-allies <akdawodu@vision-allies.com>
2026-09-15 18:50:26 -0400

    Initialized Assignment

---
+++ Assignment_2/source.cpp
@@ -0,0 +1,31 @@
+//A Program to Read Student Data
+
+#include <iostream>
+using namespace std;
+
+//Every student has a first and last name
+struct STUDENT_DATA {
+	string firstName;
+	string lastName;	
+};
+
+//Function to open a file
+int openFile() {
+
+}
+
+//Function to read a line of data
+int readDataLine() {
+
+}
+
+//Function to read an entire file
+int readFile() {
+
+}
+
+
+int main(void) {
+
+	return 0;
+}
